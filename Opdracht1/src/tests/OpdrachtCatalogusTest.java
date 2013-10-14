4d8d64aa0dd9bc93d1fb4d1f069bb835c84e170b
FireRedThorn <mr.rednas@gmail.com>
2013-10-14 18:57:09 +0200

    verdere implementatie OpdrachtCatalogusTest

--- Opdracht1/src/tests/OpdrachtCatalogusTest.java
+++ Opdracht1/src/tests/OpdrachtCatalogusTest.java
@@ -1,6 +1,4 @@
 package tests;
-import model.OpdrachtCatalogus;
-import model.Opdracht;
 
 import static org.junit.Assert.*;
 
@@ -97,17 +95,17 @@ public class OpdrachtCatalogusTest {
 	}
 
 	@Test
-	public void testGetOpdracht() {
+	public void test_GetOpdracht() {
 		fail("Not yet implemented");
 	}
 
 	@Test
-	public void testGetCatalogus() {
+	public void test_GetCatalogus() {
 		fail("Not yet implemented");
 	}
 
 	@Test
-	public void testSetCatalogus() {
+	public void test_SetCatalogus() {
 		fail("Not yet implemented");
 	}
 
@@ -146,13 +144,67 @@ public class OpdrachtCatalogusTest {
 		}
 	}
 	@Test
-	public void testRemove() {
-		fail("Not yet implemented");
+	public void test_Remove_Geldige_Opdracht_Geslaagd() {
+		this.catalogus = new OpdrachtCatalogus();
+		
+		//catalogus wordt gevuld met 60 willekeurige opdrachten
+		for(int j = 0;j<60;j++){
+			String vraag = "";
+			for(int i =0;i < generator.nextInt(120);i++){
+				vraag += Character.toString(((char) generator.nextInt()));
+			}
+			String juisteAntwoord = "";
+			for(int i = 0;i < generator.nextInt(120);i++){
+				juisteAntwoord += Character.toString(((char) generator.nextInt()));
+			}
+			int maxAantalPogingen = generator.nextInt(120);
+			int maxAntwoordTijd = generator.nextInt(120);
+			Leraar auteur = Leraar.values()[generator.nextInt(4)];
+			OpdrachtCategorie categorie = OpdrachtCategorie.values()[generator.nextInt(4)];
+			String antwoordHints = "";
+			for(int i= 0;i < generator.nextInt(120);i++){
+				antwoordHints += Character.toString(((char) generator.nextInt()));
+			}
+			String antwoordHints2 = "";
+			for(int i= 0;i < generator.nextInt(120);i++){
+				antwoordHints2 += Character.toString(((char) generator.nextInt()));
+			}		
+			this.catalogus.add(new Opdracht(vraag, juisteAntwoord, maxAantalPogingen,maxAntwoordTijd, auteur, categorie,	antwoordHints,antwoordHints2));
+		}
+		//willekeurige opdracht wordt verwijderd
+		int index = generator.nextInt(60);
+		assertEquals(true,this.catalogus.remove(this.catalogus.change(index)));
 	}
 
 	@Test
-	public void testChangeInt1() {
-		fail("Not yet implemented");
+	public void test_Remove_Ongeldige_Opdracht_Niet_Geslaagd() {
+		this.catalogus = new OpdrachtCatalogus();
+		
+		//catalogus wordt gevuld met 60 willekeurige opdrachten
+		for(int j = 0;j<60;j++){
+			String vraag = "";
+			for(int i =0;i < generator.nextInt(120);i++){
+				vraag += Character.toString(((char) generator.nextInt()));
+			}
+			String juisteAntwoord = "";
+			for(int i = 0;i < generator.nextInt(120);i++){
+				juisteAntwoord += Character.toString(((char) generator.nextInt()));
+			}
+			int maxAantalPogingen = generator.nextInt(120);
+			int maxAntwoordTijd = generator.nextInt(120);
+			Leraar auteur = Leraar.values()[generator.nextInt(4)];
+			OpdrachtCategorie categorie = OpdrachtCategorie.values()[generator.nextInt(4)];
+			String antwoordHints = "";
+			for(int i= 0;i < generator.nextInt(120);i++){
+				antwoordHints += Character.toString(((char) generator.nextInt()));
+			}
+			String antwoordHints2 = "";
+			for(int i= 0;i < generator.nextInt(120);i++){
+				antwoordHints2 += Character.toString(((char) generator.nextInt()));
+			}		
+			this.catalogus.add(new Opdracht(vraag, juisteAntwoord, maxAantalPogingen,maxAntwoordTijd, auteur, categorie,	antwoordHints,antwoordHints2));
+		}
+		assertEquals(false,this.catalogus.remove(new Opdracht("testvraag", "testantwoord", 1,1, Leraar.An, OpdrachtCategorie.AlgemeneKennis,	"Hint")));
 	}
 
 }
