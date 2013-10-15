5bc1519bf590fac203c5bbf2711c8df7d7d64dec
Wim Ombelets <wim.ombelets@gmail.com>
2013-10-15 19:48:00 +0200

    eerste versie AntwoordTest

---
+++ Opdracht1/src/tests/AntwoordTest.java
@@ -0,0 +1,33 @@
+package tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+import model.Antwoord;
+
+public class AntwoordTest {
+	
+	@Before
+	public void setUp() throws Exception {
+		new Antwoord();
+	}
+	
+	@Test
+	public void test_setAntwoord_Geldige_Waarde(){
+		String antwoordWaarde = "blabla";
+		new Antwoord(antwoordWaarde);
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void test_setAntwoord_Ongeldige_Null_Waarde(){
+		String antwoordWaarde = null;
+		new Antwoord(antwoordWaarde);		
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void test_setAntwoord_Ongeldige_EmptyString_Waarde(){
+		String antwoordWaarde = "";
+		new Antwoord(antwoordWaarde);
+	}
+}
