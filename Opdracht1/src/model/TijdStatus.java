c5160472ff8c079680d319165bc84988b54ceb0c
Wim Ombelets <wim.ombelets@gmail.com>
2013-10-15 20:22:11 +0200

    Added TijdStatus as described in Toledo folder Opdracht3

---
+++ Opdracht1/src/model/TijdStatus.java
@@ -0,0 +1,32 @@
+package model;
+import java.util.*;
+
+public enum TijdStatus {
+	VROEG("Voor 8 uur"),
+	LAAT("Na 21 uur"),
+	MEDIUM("Tussen 8 uur en 21 uur");
+	
+	private final String momentVanDag;
+	
+	private String getMomentVanDag(){
+		return momentVanDag;
+	}
+	
+/*	public String toString(){
+		return this.name().toLowerCase();
+	}*/
+	
+	TijdStatus(String momentVanDag){
+		this.momentVanDag = momentVanDag;
+	}
+	
+	public static void main(String[] args) {
+		for (TijdStatus tijdStatus : TijdStatus.values()){
+			System.out.println(tijdStatus+","+tijdStatus.getMomentVanDag());
+			TijdStatus ts = TijdStatus.MEDIUM;
+			
+		}
+			
+	}
+
+}
