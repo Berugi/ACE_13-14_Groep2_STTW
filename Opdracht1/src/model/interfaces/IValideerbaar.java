d0c488e5d9ab8fa0de81609c554a65b40b7d18c2
Wim Ombelets <wim.ombelets@gmail.com>
2013-10-20 15:04:40 +0200

    added version info

--- Opdracht1/src/model/interfaces/IValideerbaar.java
+++ Opdracht1/src/model/interfaces/IValideerbaar.java
@@ -1,5 +1,11 @@
 package model.interfaces;
 
+/**
+ * 
+ * @author wim ombelets
+ * @version 20131014-01 initial commit
+ *
+ */
 public interface IValideerbaar {
 	public boolean isValide(String antwoord);
 	public String getValideerTekst();
