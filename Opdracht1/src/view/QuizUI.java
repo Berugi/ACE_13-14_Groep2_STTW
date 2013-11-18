e51a7b7b2e4422c5fc62d79d77d2adea1a6b0b5f
Flying Wizard <on1dru@netscape.net>
2013-11-18 18:21:29 +0100

    Quiz UI update

--- Opdracht1/src/view/QuizUI.java
+++ Opdracht1/src/view/QuizUI.java
@@ -43,76 +43,96 @@ public class QuizUI extends JFrame {
 		super.setContentPane(mainPanel);
 		mainPanel.setLayout(null);
 		
-		JPanel panel = new JPanel();
-		panel.setBorder(new LineBorder(new Color(0, 0, 0)));
-		panel.setBounds(0, 116, 843, 422);
-		mainPanel.add(panel);
-		panel.setLayout(null);
+		JPanel bottomPanel = new JPanel();
+		bottomPanel.setBorder(new LineBorder(new Color(0, 0, 0)));
+		bottomPanel.setBounds(0, 116, 843, 422);
+		mainPanel.add(bottomPanel);
+		bottomPanel.setLayout(null);
 		
 		JComboBox comboBox = new JComboBox();
 		comboBox.setBounds(141, 23, 117, 22);
-		panel.add(comboBox);
+		bottomPanel.add(comboBox);
 		
 		JComboBox comboBox_1 = new JComboBox();
 		comboBox_1.setBounds(141, 81, 117, 22);
-		panel.add(comboBox_1);
+		bottomPanel.add(comboBox_1);
 		
 		JButton btnNewButton_1 = new JButton("^^^^^^^^^");
 		btnNewButton_1.setFont(new Font("Verdana", Font.PLAIN, 14));
-		btnNewButton_1.setBounds(538, 81, 148, 40);
-		panel.add(btnNewButton_1);
+		btnNewButton_1.setBounds(574, 90, 148, 40);
+		bottomPanel.add(btnNewButton_1);
 		
 		JButton btnNewButton_2 = new JButton(">>>");
 		btnNewButton_2.setBounds(370, 143, 97, 40);
-		panel.add(btnNewButton_2);
+		bottomPanel.add(btnNewButton_2);
 		
 		JButton btnNewButton_3 = new JButton("<<<");
 		btnNewButton_3.setBounds(370, 186, 97, 40);
-		panel.add(btnNewButton_3);
+		bottomPanel.add(btnNewButton_3);
 		
-		JPanel panel_1 = new JPanel();
-		panel_1.setBorder(new LineBorder(new Color(0, 0, 0)));
-		panel_1.setBounds(0, 0, 843, 103);
-		mainPanel.add(panel_1);
-		panel_1.setLayout(null);
+		JLabel lblCategorie = new JLabel("categorie :");
+		lblCategorie.setBounds(12, 26, 117, 16);
+		bottomPanel.add(lblCategorie);
+		
+		JLabel lblNewLabel = new JLabel("New label");
+		lblNewLabel.setBounds(538, 41, 56, 16);
+		bottomPanel.add(lblNewLabel);
+		
+		JLabel lblNewLabel_1 = new JLabel("New label");
+		lblNewLabel_1.setBounds(12, 84, 56, 16);
+		bottomPanel.add(lblNewLabel_1);
+		
+		JList list = new JList();
+		list.setBounds(22, 143, 324, 253);
+		bottomPanel.add(list);
+		
+		JList list_1 = new JList();
+		list_1.setBounds(493, 143, 324, 266);
+		bottomPanel.add(list_1);
+		
+		JPanel topPanel = new JPanel();
+		topPanel.setBorder(new LineBorder(new Color(0, 0, 0)));
+		topPanel.setBounds(0, 0, 843, 103);
+		mainPanel.add(topPanel);
+		topPanel.setLayout(null);
 		
 		lblOnderwerp = new JLabel("Onderwerp :");
 		lblOnderwerp.setBounds(12, 9, 73, 16);
-		panel_1.add(lblOnderwerp);
+		topPanel.add(lblOnderwerp);
 		
 		klasComboBox = new JComboBox();
 		klasComboBox.setModel(new DefaultComboBoxModel(new String[] {"", "1A", "2A", "3A", "4A", "5A", "6A"}));
 		klasComboBox.setBounds(367, 6, 62, 22);
-		panel_1.add(klasComboBox);
+		topPanel.add(klasComboBox);
 		
 		textField = new JTextField();
 		textField.setBounds(97, 6, 215, 22);
-		panel_1.add(textField);
+		topPanel.add(textField);
 		textField.setColumns(10);
 		
 		lblKlas = new JLabel("Klas : ");
 		lblKlas.setBounds(324, 9, 36, 16);
-		panel_1.add(lblKlas);
+		topPanel.add(lblKlas);
 		
 		auteurComboBox = new JComboBox(Leraar.values());
 		auteurComboBox.setBounds(494, 6, 100, 22);
-		panel_1.add(auteurComboBox);
+		topPanel.add(auteurComboBox);
 		
 		lblAuteur = new JLabel("Auteur : ");
 		lblAuteur.setBounds(442, 9, 51, 16);
-		panel_1.add(lblAuteur);
+		topPanel.add(lblAuteur);
 		
 		btnNewButton = new JButton("Registreer nieuwe quiz");
 		btnNewButton.setBounds(12, 56, 819, 34);
-		panel_1.add(btnNewButton);
+		topPanel.add(btnNewButton);
 		
 		lblQuizStatus = new JLabel("Quiz Status :");
 		lblQuizStatus.setBounds(606, 9, 74, 16);
-		panel_1.add(lblQuizStatus);
+		topPanel.add(lblQuizStatus);
 		
 		JComboBox quizStatusComboBox = new JComboBox(QuizStatus.values());
 		quizStatusComboBox.setBounds(692, 6, 125, 22);
-		panel_1.add(quizStatusComboBox);
+		topPanel.add(quizStatusComboBox);
 		btnNewButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 			}
