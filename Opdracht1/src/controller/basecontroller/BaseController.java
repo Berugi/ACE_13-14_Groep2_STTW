6cd76b5f6b5e9e290e6342e31824223cb63bf666
ombeletw <wim.ombelets@gmail.com>
2013-11-22 14:47:49 +0100

    BaseController initial commit

---
+++ Opdracht1/src/controller/basecontroller/BaseController.java
@@ -0,0 +1,14 @@
+package controller.basecontroller;
+
+/**
+ * The BaseController is the base class that abstracts general
+ * functionality required by all other (conctrete) controllers.
+ * 
+ * @author Wim Ombelets
+ * @version 20131122-01 initial commit
+ * 
+ * 
+ */
+public abstract class BaseController {
+
+}
