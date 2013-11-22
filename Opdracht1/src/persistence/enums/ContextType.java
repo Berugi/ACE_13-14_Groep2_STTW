359f839791f4efd8d2b253e2fffafdf30453e90b
ombeletw <wim.ombelets@gmail.com>
2013-11-22 18:39:01 +0100

    Added ContextType

---
+++ Opdracht1/src/persistence/enums/ContextType.java
@@ -0,0 +1,14 @@
+package persistence.enums;
+
+/**
+ * The types of persistence contexts.
+ * 
+ * @author Wim Ombelets
+ * @version 20131122-01 - initial commit
+ *
+ */
+public enum ContextType {
+	Local,
+	Database,
+	DatabaseAndLocal
+}
