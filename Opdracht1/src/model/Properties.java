ddc75b93f96d9b0bea3f18210fd66b853b2974f8
ombeletw <wim.ombelets@gmail.com>
2013-11-23 23:13:03 +0100

    Added empty Properties class

---
+++ Opdracht1/src/model/Properties.java
@@ -0,0 +1,18 @@
+package model;
+
+/**
+ * Can read from and write to the start.ini file.
+ * Important to know, is that this class does not use nor extend java.util.Properties.
+ * 
+ * @author Wim Ombelets
+ * @version 20131123-01 - initial commit
+ *
+ */
+public class Properties {
+	
+	//constructors
+	
+	public Properties() {
+		// TODO Auto-generated constructor stub
+	}
+}
