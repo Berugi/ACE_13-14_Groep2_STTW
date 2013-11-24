092c3055afd5248e437a7941390352acf63d50f8
ombeletw <wim.ombelets@gmail.com>
2013-11-24 19:40:44 +0100

    Added version info and comment

--- Opdracht1/src/model/enums/DBType.java
+++ Opdracht1/src/model/enums/DBType.java
@@ -1,5 +1,13 @@
 package model.enums;
 
+/**
+ * Enumerates possible database types.
+ * 
+ * @author Wim Ombelets
+ * @version 20131124-01 initial commit
+ * 
+ */
 public enum DBType {
+	//ALL VALUES NEED to BE IN CAPS
 	MYSQL
 }
