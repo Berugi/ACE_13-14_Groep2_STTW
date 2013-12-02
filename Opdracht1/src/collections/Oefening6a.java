21196c6521ba65a9261a51c8cc9504f81c7b3ec9
Flying Wizard <on1dru@netscape.net>
2013-12-02 21:15:54 +0100

    Comments voor oefening 6a

--- Opdracht1/src/collections/Oefening6a.java
+++ Opdracht1/src/collections/Oefening6a.java
@@ -1,10 +1,15 @@
 package collections;
 
+import model.OpdrachtCatalogus;
+
 public class Oefening6a {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		//inlezen opdrachten categorieen in Hashset
+		//inlezen van opdrachten vanuit tekstbestand in een Hashset
+		
+		OpdrachtCatalogus OpdrachtCat = new OpdrachtCatalogus();
 	}
 
 }
