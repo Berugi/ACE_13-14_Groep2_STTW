90b5c500d8087f838636e97c8e1fd3faec950e67
Flying Wizard <on1dru@netscape.net>
2013-12-02 20:56:51 +0100

    Updated opdrachtencategorie volgens opdracht doc.

--- Opdracht1/src/collections/Oefening6b.java
+++ Opdracht1/src/collections/Oefening6b.java
@@ -1,5 +1,9 @@
 package collections;
 
 public class Oefening6b {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
 
+	}
 }
--- Opdracht1/src/model/enums/OpdrachtCategorie.java
+++ Opdracht1/src/model/enums/OpdrachtCategorie.java
@@ -1,9 +1,26 @@
 package model.enums;
 
 public enum OpdrachtCategorie {
-	TBA,
-	Rekenen,
-	NederlandseNaam,
-	FranseNaam,
-	AlgemeneKennis
+	TBA(" "),
+	WISKUNDE("Wiskunde"),
+	AARDRIJKSKUNDE("Aardrijkskunde"),
+	NEDERLANDS("Nederlands"),
+	WETENSCHAPPEN("Wetenschappen");
+	
+	private final String description;
+
+	public String getDescription()
+	{
+		return description;
+	}
+
+	private OpdrachtCategorie(String description)
+	{
+		this.description = description;
+	}
+	
+	@Override
+    public String toString() {
+        return description;
+    }
 }
