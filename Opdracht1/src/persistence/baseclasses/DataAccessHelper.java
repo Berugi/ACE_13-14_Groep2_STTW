a93a15de73a43add37742b26b4825cfcb5242a3e
ombeletw <wim.ombelets@gmail.com>
2013-12-09 19:49:09 +0100

    updated DataAccessHelper

---
+++ Opdracht1/src/persistence/baseclasses/DataAccessHelper.java
@@ -0,0 +1,13 @@
+package persistence.baseclasses;
+
+/**
+ * 
+ * @author Wim Ombelets
+ * @version 20131209-01 - initial commit
+ * 
+ * This is, for now, a dummy base class that all other DataAccessHelper classes extend from
+ *
+ */
+public class DataAccessHelper {
+
+}
