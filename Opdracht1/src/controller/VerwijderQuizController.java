a31177f40b015233f8e358068ddea885e5d79143
ombeletw <wim.ombelets@gmail.com>
2013-12-09 19:21:16 +0100

    refactor using IRepositoryDelete<Quiz>

--- Opdracht1/src/controller/VerwijderQuizController.java
+++ Opdracht1/src/controller/VerwijderQuizController.java
@@ -1,21 +1,32 @@
 package controller;
 
+import java.util.HashSet;
+
+import persistence.enums.ContextType;
+import persistence.interfaces.IRepositoryDelete;
 import model.Quiz;
 
 /**
  * 
  * @author Wim Ombelets
  * @version 20131201-01 - initial commit
+ * @version 20131209-01 - refactor using {@link IRepositoryDelete}
  *
  */
-public class VerwijderQuizController {
+public class VerwijderQuizController implements IRepositoryDelete<Quiz> {
 	
-	public VerwijderQuizController() {
+	public VerwijderQuizController(ContextType type) {
 		// TODO Auto-generated constructor stub
 	}
-	
-	public void verwijderen(Quiz q) {
-		//TODO verder afwerken
+
+	public boolean delete(Quiz t) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public boolean deleteSubset(HashSet<Quiz> subset) {
+		// TODO Auto-generated method stub
+		return false;
 	}
 
 }
