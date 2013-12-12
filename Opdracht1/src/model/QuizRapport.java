1759e018c8ed1ab20e2323570946915b2424ff2f
ombeletw <wim.ombelets@gmail.com>
2013-12-12 19:17:06 +0100

    Had forgotten to make QuizRapport implement IQuizRapport

--- Opdracht1/src/model/QuizRapport.java
+++ Opdracht1/src/model/QuizRapport.java
@@ -1,11 +1,17 @@
 package model;
 
+import model.interfaces.IQuizRapport;
+
 /**
  * 
  * @author Wim Ombelets
  * @version 20131212-01 - initial commit
  *
  */
-public class QuizRapport {
+public class QuizRapport implements IQuizRapport {
+
+	public String toonRapport() {
+		return this.toString();
+	}
 
 }
