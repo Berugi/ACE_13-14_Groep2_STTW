18c4952bb422ac484cec0e0975d615f4d61ce359
ombeletw <wim.ombelets@gmail.com>
2013-12-13 22:05:49 +0100

    Removed TODO's and added javadoc info

--- Opdracht1/src/model/CSVSerializer.java
+++ Opdracht1/src/model/CSVSerializer.java
@@ -4,41 +4,42 @@ import persistence.interfaces.ICSVSerializable;
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 import model.baseclasses.AbstractCSVSerializer;
 
+/**
+ * Concrete CSV Serializer class that can switch serializer strategy based on certain class types.
+ * 
+ * @author Wim Ombelets
+ * @version 20131213-01 - initial commit
+ *
+ */
 public class CSVSerializer extends AbstractCSVSerializer {
 
 	@Override
 	public Character getPropertySeparator() {
-		// TODO Auto-generated method stub
 		return super.getPropertySeparator();
 	}
 
 	@Override
 	public void setPropertySeparator(Character propertySeparator) {
-		// TODO Auto-generated method stub
 		super.setPropertySeparator(propertySeparator);
 	}
 
 	@Override
 	public ICSVSerializable<Object> getSerializerStrategy() {
-		// TODO Auto-generated method stub
 		return super.getSerializerStrategy();
 	}
 
 	@Override
 	public void setSerializerStrategy(Object o) throws NotImplementedException {
-		// TODO Auto-generated method stub
 		super.setSerializerStrategy(o);
 	}
 
 	@Override
 	public String serialize(Object o) {
-		// TODO Auto-generated method stub
 		return super.serialize(o);
 	}
 
 	@Override
 	public Object deserialize(String csvString) {
-		// TODO Auto-generated method stub
 		return super.deserialize(csvString);
 	}
 
