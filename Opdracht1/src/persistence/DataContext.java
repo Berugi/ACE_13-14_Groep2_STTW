f8ae3c047c6b456959c254b10f234dd81bd190da
ombeletw <wim.ombelets@gmail.com>
2013-12-28 23:15:04 +0100

    DataContext now uses PersistenceStrategyFatory to set its strategy

--- Opdracht1/src/persistence/DataContext.java
+++ Opdracht1/src/persistence/DataContext.java
@@ -1,7 +1,16 @@
 package persistence;
 
-import persistence.interfaces.IPersistenceStrategy;;
+import persistence.enums.ContextType;
+import persistence.interfaces.IPersistenceStrategy;
 
+/**
+ * Represents a container for a persistence strategy
+ * 
+ * @author Tom Scheepers
+ * @version 20131221-01 - initial commit
+ * @version 20131228 - modified by Wim Ombelets - now uses PersistenceStrategyFactory to set the strategy.
+ *
+ */
 public class DataContext {
 	
 	private IPersistenceStrategy dataStrategie = null;
@@ -12,6 +21,26 @@ public class DataContext {
 	
 	public void setStrategy(IPersistenceStrategy st){
 		this.dataStrategie = st;
+	}
+	
+	/**
+	 * De default constructor zal de persistence strategy uit de ini file halen
+	 */
+	public DataContext() {
+		
+		setStrategy(PersistenceStrategyFactory.createPersistenceStrategy());
+		
+	}
+	
+	/**
+	 * Deze constructor zal de persistence strategy instellen op basis van een ContextType.
+	 * 
+	 * @param contextType
+	 */
+	public DataContext(ContextType contextType) {
+		
+		setStrategy(PersistenceStrategyFactory.createPersistenceStrategy(contextType));
+		
 	}
 
 }
