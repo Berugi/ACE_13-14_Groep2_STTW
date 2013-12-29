3e1f45174987b448bed0b8345d4af4f6ea37a167
Tom Scheepers <tom.scheepers@telenet.be>
2013-12-29 17:11:30 +0100

    DataTest class (menu option 8) : show all quizen / Opdrachten

---
+++ Opdracht1/src/tests/model/DataTest.java
@@ -0,0 +1,23 @@
+package tests.model;
+
+import model.ObservableOpdrachtCatalogus;
+import model.ObservableQuizCatalogus;
+import model.Quiz;
+import model.baseclasses.Opdracht;
+
+public class DataTest {
+	
+	public DataTest(ObservableQuizCatalogus qc, ObservableOpdrachtCatalogus oc){
+		
+		System.out.println("\nQuizen:\n");
+		for(Quiz quiz: qc.quizen){
+			System.out.println(quiz.toString());
+		}
+
+		System.out.println("\nOpdrachten:\n");
+		for(Opdracht opdracht: oc.getCatalogus()){
+			System.out.println(opdracht.toString());
+		}
+	}
+
+}
