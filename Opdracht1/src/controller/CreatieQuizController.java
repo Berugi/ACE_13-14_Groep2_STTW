1b06389188399e8afc440a1407833ac4e932f47a
Wim Ombelets <wim.ombelets@gmail.com>
2014-01-04 03:12:25 +0100

    Worked on CreatieQuizController

--- Opdracht1/src/controller/CreatieQuizController.java
+++ Opdracht1/src/controller/CreatieQuizController.java
@@ -1,5 +1,14 @@
 package controller;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Arrays;
+
+import model.ObservableQuizCatalogus;
+import model.OpdrachtActionEvent;
+import model.Quiz;
+import model.QuizActionEvent;
+import model.baseclasses.Opdracht;
 import model.interfaces.IObservable;
 import persistence.DataContext;
 import persistence.enums.ContextType;
@@ -13,7 +22,7 @@ import view.CreatieQuizView;
  * @version 20131228-01 - added beginnings of link to model and view
  *
  */
-public class CreatieQuizController {
+public class CreatieQuizController implements ActionListener {
 	
 	private CreatieQuizController instance = null;
 	private DataContext dc;
@@ -21,6 +30,8 @@ public class CreatieQuizController {
 	private CreatieQuizView view;
 	private IObservable quizCatalogusModel;
 	private IObservable opdrachtCatalogusModel;
+	
+	private final String[] quizCommands = {"addQuiz", "deleteQuiz"};
 	
 	private CreatieQuizController(ContextType contextType) {
 		
@@ -48,6 +59,43 @@ public class CreatieQuizController {
 	public void hideView() {
 		
 		this.view.setVisible(false);
+		
+	}
+
+	public void actionPerformed(ActionEvent e) throws NullPointerException, IllegalArgumentException {
+		
+		if(e == null)
+			throw new NullPointerException();
+		
+		if(Arrays.asList(quizCommands).contains(e.getActionCommand())){
+			
+			QuizActionEvent event = (QuizActionEvent)e;
+			Quiz q = event.getEventData();
+			
+			switch(e.getActionCommand()) {
+			case "addQuiz":
+				addQuiz(q);
+				break;
+			case "deleteQuiz":
+				removeQuiz(q);
+				break;
+			}
+			
+		}		
+		else
+			throw new IllegalArgumentException("Illegal type of ActionEvent.");
+		
+	}
+	
+	private void addQuiz(Quiz q) {
+		
+		((ObservableQuizCatalogus) quizCatalogusModel).add(q);
+		
+	}
+	
+	private void removeQuiz(Quiz q) {
+		
+		((ObservableQuizCatalogus) quizCatalogusModel).remove(q);
 		
 	}
 
