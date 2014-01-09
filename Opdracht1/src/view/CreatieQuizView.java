df9ea80cf1cc41b76507c64d01871b02adf4b938
Tom Scheepers <tom.scheepers@telenet.be>
2014-01-09 23:18:27 +0100

    CreateQuizView aangepast zodat het gelinkt kan worden met QuizApp

--- Opdracht1/src/view/CreatieQuizView.java
+++ Opdracht1/src/view/CreatieQuizView.java
@@ -1,25 +1,24 @@
 package view;
 
-import java.awt.EventQueue;
-
 import javax.swing.DefaultComboBoxModel;
-import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JTextField;
 import javax.swing.JCheckBox;
 import javax.swing.JComboBox;
 import javax.swing.JButton;
-import javax.swing.ButtonGroup;
+import javax.swing.JPanel;
 
-import controller.CreatieQuizController;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 
 import model.Quiz;
 import model.enums.Leraar;
 
-public class CreatieQuizView extends JFrame implements ActionListener {
+import java.util.Observable;
+import java.util.Observer;
+
+public class CreatieQuizView extends JPanel implements Observer, ActionListener {
 	
 	private static final long serialVersionUID = -3041414196751715178L;
 	private JTextField textFieldOnderwerp;
@@ -39,10 +38,12 @@ public class CreatieQuizView extends JFrame implements ActionListener {
 	private JLabel lblQuizOpdrachten;
 	private JButton btnOpslaan;
 	private JButton btnWissen;
+	private ActionListener listener;
 
 	/**
 	 * Launch the application.
 	 */
+	/*
 	public static void main(String[] args) {
 		
 		EventQueue.invokeLater(new Runnable() {
@@ -57,10 +58,17 @@ public class CreatieQuizView extends JFrame implements ActionListener {
 		});
 		
 	}
-
-	/**
-	 * Create the frame.
-	 */
+	*/
+	//getters & setters
+	public ActionListener getActionListener(){
+		return listener;
+	}
+	
+	public void setActionListener(ActionListener listener){
+		this.listener = listener;
+	}
+	
+	// Constructor
 	public CreatieQuizView() {
 		
 		initialize();
@@ -69,92 +77,90 @@ public class CreatieQuizView extends JFrame implements ActionListener {
 	
 	private void initialize() {
 		
-		setTitle("Create Quiz");
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 471, 421);
-		setLocationRelativeTo(null);
-		setResizable(false);
-		getContentPane().setLayout(null);
+		setLayout(null);
 		
 		JLabel lblOnderwerp = new JLabel("Onderwerp");
 		lblOnderwerp.setBounds(10, 14, 86, 14);
-		getContentPane().add(lblOnderwerp);
+		add(lblOnderwerp);
 		
 		textFieldOnderwerp = new JTextField();
 		textFieldOnderwerp.setBounds(116, 11, 326, 20);
-		getContentPane().add(textFieldOnderwerp);
+		add(textFieldOnderwerp);
 		textFieldOnderwerp.setColumns(10);
 		
 		lblLeerjaren = new JLabel("Leerjaren");
 		lblLeerjaren.setBounds(10, 39, 86, 14);
-		getContentPane().add(lblLeerjaren);
+		add(lblLeerjaren);
 		
 		chkBox1eJaar = new JCheckBox("1e jaar");
 		chkBox1eJaar.setBounds(116, 35, 70, 23);
-		getContentPane().add(chkBox1eJaar);
+		add(chkBox1eJaar);
 		
 		chkBox2eJaar = new JCheckBox("2e jaar");
 		chkBox2eJaar.setBounds(244, 35, 70, 23);
-		getContentPane().add(chkBox2eJaar);
+		add(chkBox2eJaar);
 		
 		chkBox3eJaar = new JCheckBox("3e jaar");
 		chkBox3eJaar.setBounds(372, 35, 70, 23);
-		getContentPane().add(chkBox3eJaar);
+		add(chkBox3eJaar);
 		
 		chkBox4eJaar = new JCheckBox("4e jaar");
 		chkBox4eJaar.setBounds(116, 61, 70, 23);
-		getContentPane().add(chkBox4eJaar);
+		add(chkBox4eJaar);
 		
 		chkBox5eJaar = new JCheckBox("5e jaar");
 		chkBox5eJaar.setBounds(244, 61, 70, 23);
-		getContentPane().add(chkBox5eJaar);
+		add(chkBox5eJaar);
 		
 		chkBox6eJaar = new JCheckBox("6e jaar");
 		chkBox6eJaar.setBounds(372, 61, 70, 23);
-		getContentPane().add(chkBox6eJaar);
+		add(chkBox6eJaar);
 		
 		chkBoxIsTest = new JCheckBox("Is Test");
 		chkBoxIsTest.setBounds(10, 102, 70, 23);
-		getContentPane().add(chkBoxIsTest);
+		add(chkBoxIsTest);
 		
 		chkBoxIsUniekeDeelname = new JCheckBox("Is Unieke Deelname");
 		chkBoxIsUniekeDeelname.setBounds(116, 102, 145, 23);
-		getContentPane().add(chkBoxIsUniekeDeelname);
+		add(chkBoxIsUniekeDeelname);
 		
 		comboBoxLeraar = new JComboBox<Leraar>();
 		comboBoxLeraar.setModel(new DefaultComboBoxModel<Leraar>(Leraar.values()));
 		comboBoxLeraar.setBounds(150, 132, 128, 20);
-		getContentPane().add(comboBoxLeraar);
+		add(comboBoxLeraar);
 		
 		lblLeraar = new JLabel("Leraar");
 		lblLeraar.setBounds(10, 135, 76, 14);
-		getContentPane().add(lblLeraar);
+		add(lblLeraar);
 		
 		lblDatum = new JLabel("Datum (dd/MM/yyyy)");
 		lblDatum.setBounds(10, 160, 130, 14);
-		getContentPane().add(lblDatum);
+		add(lblDatum);
 		
 		textField = new JTextField();
 		textField.setColumns(10);
 		textField.setBounds(150, 157, 164, 20);
-		getContentPane().add(textField);
+		add(textField);
 		
 		lblQuizOpdrachten = new JLabel("Quiz Opdrachten");
 		lblQuizOpdrachten.setBounds(10, 185, 97, 14);
-		getContentPane().add(lblQuizOpdrachten);
+		add(lblQuizOpdrachten);
 		
 		btnOpslaan = new JButton("Opslaan");
 		btnOpslaan.setBounds(334, 349, 108, 23);
+		btnOpslaan.setActionCommand("addQuiz");
 		btnOpslaan.addActionListener(this);
-		getContentPane().add(btnOpslaan);
+		add(btnOpslaan);
 		
 		btnWissen = new JButton("Wissen");
 		btnWissen.setBounds(217, 349, 108, 23);
+		btnWissen.setActionCommand("deleteQuiz");
 		btnWissen.addActionListener(this);
-		getContentPane().add(btnWissen);
-		
+		add(btnWissen);
+		setVisible(true);
 	}
 	
+
 	public void actionPerformed (ActionEvent e) {
 		
 		if(e.getSource() == btnWissen)
@@ -163,7 +169,6 @@ public class CreatieQuizView extends JFrame implements ActionListener {
 			ingaveOpslaan();
 		
 	}
-	
 	private void ingaveWissen() {
 		JOptionPane.showMessageDialog(null, "btnWissen werd geklikt");
 	}
@@ -172,10 +177,9 @@ public class CreatieQuizView extends JFrame implements ActionListener {
 		//JOptionPane.showMessageDialog(null, "btnOpslaan werd geklikt");
 		
 		Quiz q = valideer();
-		
 		if(q != null) {
-			CreatieQuizController controller = new CreatieQuizController();
-			controller.opslaan(q);
+			//CreatieQuizController controller = new CreatieQuizController();
+			//controller.opslaan(q);
 		}
 		else {
 			JOptionPane.showMessageDialog(null, "valideer method not yet implemented");
@@ -190,4 +194,9 @@ public class CreatieQuizView extends JFrame implements ActionListener {
 		
 		return null;
 	}
+	
+	@Override
+	public void update(Observable changedModel, Object arg1) {
+			
+		}
 }
